4d7097c585dfe43fab93f7248ba39ea0b0418f27
juliaandreds <julia.andre@hotmail.com>
2023-01-23 14:08:34 -0300

    Projeto livraria

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use (express.json());
+
 const livros = [
     {id: 1, titulo: "Senhos dos Anéis"},
     {id: 2, título: "Hobbit"}
@@ -13,6 +15,11 @@ app.get ('/', (req,res) => {
 
 app.get ('/livros', (req,res) => {
     res.status (200).json(livros);
+})
+
+app.post ('/livros', (req,res) => {
+    livros.push (req.body);
+    res.status(201).send('livro cadastrado com sucesso')    
 })
 
 export default app
